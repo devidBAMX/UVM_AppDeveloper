cece62e65f5434791919e25b423884bf8a474f9d
devidBAMX <ddominguez@bamx.org.mx>
2025-01-18 11:07:11 -0600

    Organización de carpetas y archivos para estructuras de control

---
+++ 1-Introducción al desarrollo de aplicaciones móviles/Switch-Case/switch-case.js
@@ -0,0 +1,23 @@
+//Estructura del switch
+
+
+switch (expresion) { //expresion es la variable que se va a evaluar
+    case valor1:
+        //Código que se ejecuta si la expresión es igual a valor1
+        console.log('El valor es 1');
+        break;
+    case valor2:
+        //Código que se ejecuta si la expresión es igual a valor2
+        console.log('El valor es 2');
+        break;
+    case valor3:
+        //Código que se ejecuta si la expresión es igual a valor3
+        console.log('El valor es 2');
+        break;
+
+    default:
+        //Código que se ejecuta si la expresión no es igual a ninguno de los valores
+        console.log('El valor no es 1, 2 o 3');
+
+        break;
+    }
